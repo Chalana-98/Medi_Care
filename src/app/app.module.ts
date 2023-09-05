d9e1b582d2c0fdef0fc3efd63c1dc7c66deb806f
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2023-09-05 20:44:59 +0530

    d0ctor-form
    add doctor-form to fill medical records

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MyMaterialModule} from "./material";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DoctorFormComponent } from './doctor-form/doctor-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorFormComponent
   ],
   imports: [
     BrowserModule,
